947e0b50c5cb8dd10632f23985bfa478b46b49a5
nawa-ton <nawaratt.ton@gmail.com>
2021-06-25 10:29:38 -0700

    Implement backend for Stripe credit card payment

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountRouter = require('./routes/account');
+var paymentRouter = require('./routes/payment');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', accountRouter);
+app.use('/payment', paymentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
