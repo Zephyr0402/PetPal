4d0ae5f1d763d89d075bfd958723879176d34310
nawa-ton <nawaratt.ton@gmail.com>
2021-06-25 17:10:58 -0700

    Update /payment to /api/payment

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', accountRouter);
-app.use('/payment', paymentRouter);
+app.use('/api/payment', paymentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
