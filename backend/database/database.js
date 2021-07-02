987444d556506136a3742af69aa4e1fd980cbf58
ZephyrOne <bjshenshijun@yahoo.com>
2021-07-01 21:15:21 -0700

    Fetched data from database for animal list

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,29 +1,11 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
 const configs = {
     URL: '127.0.0.1:27017',
     Name: 'PetPal',
 }
 
-const animalInfoSchema = new Schema({
-    id: Number,
-    name: String,
-    image: String,
-    age: String,
-    price: Number,
-    user: String,
-    userAvatar: String,
-    king: String,
-    description: String,
-    address: String,
-    position: {
-        lat: String,
-        lng: String,
-    },
-    // available or sold
-    status: String,
-});
+mongoose.connect('mongodb://' + configs.URL + '/' + configs.Name);
 
 exports.configs = configs;
-exports.animalInfoSchema = animalInfoSchema;
+exports.mongoose = mongoose;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/users', {
-    useNewUrlParser: true
-})
+// mongoose.connect('mongodb://127.0.0.1:27017/users', {
+//     useNewUrlParser: true
+// })
 
 const User = mongoose.model('User', new mongoose.Schema({
     uuid: {type: String},
