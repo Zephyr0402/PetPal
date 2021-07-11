8997f10ec7e787a93eae3445675caea31fc3f631
LiJu21 <li.ju21@foxmail.com>
2021-07-10 23:58:30 -0700

    could not update userinfo

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -13,7 +13,11 @@ const UserInfo = database.model('UserInfo', new database.Schema({
     uuid: {type: String},
     name: {type: String},
     email: {type: String},
-    avatar: {type: String}
+    avatar: {type: String},
+    phone: {type: String},
+    city: {type: String},
+    intro: {type: String},
+    rating: {type: String},
 }))
 
 const UserAuth = database.model('UserAuth', new database.Schema({
