810023e7fac3319ffc4dff58fa48412f42fc9a5f
nawa-ton <nawaratt.ton@gmail.com>
2021-07-14 19:49:12 -0700

    Get transaction's sellerId from animalinfo, test for transaction status update, update prop

--- frontend/src/Payment/OrderConfirmation.js
+++ frontend/src/Payment/OrderConfirmation.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import {SmileTwoTone, CheckCircleTwoTone} from "@ant-design/icons";
-import {Typography} from "antd";
+import {Typography, Button} from "antd";
+import {CardElement} from "@stripe/react-stripe-js";
 
 const { Title, Text, Link } = Typography;
+const backEndURL = "http://localhost:9999/api/";
 
 function OrderConfirmation(props) {
+
+    const handleCancel = async (e) => {
+        e.preventDefault();
+
+        const transaction = {
+            id: props.newTransaction._id,
+            status: "canceled"
+        };
+
+        fetch(backEndURL + "transaction/update_status", {
+            method: "PATCH",
+            headers: new Headers({
+                'Accept': 'application/json',
+                "Content-Type": "application/json",
+            }),
+            body: JSON.stringify(transaction)
+        }).then(res => {
+            if(res.status === 200) {
+                console.log("transaction status successfully updated");
+            }else if(res.status >= 400){
+                console.log("transaction status fail to updated");
+            }
+        });
+    };
 
     return (
         <div className="order-confirmation">
             <CheckCircleTwoTone twoToneColor="#00b8b0" />
             <Title level={3}>Successful Payment</Title>
             <div className="order-info">
-                <Title level={4}>Order Number: {props.orderNumber}</Title>
+                <Title level={4}>Order Number: {props.newTransaction.orderNumber}</Title>
                 <Text>{new Date().toLocaleString('en-CA')}</Text>
                 <hr/>
                 <Title level={4} className="payment-summary"><span>{props.animal.name}</span><span>${props.animal.price}</span></Title>
@@ -22,9 +48,14 @@ function OrderConfirmation(props) {
                 <Text>Have a lovely day <SmileTwoTone /></Text>
             </div>
 
-            <Link href="http://localhost:3000/user">
+            <Link href="http://localhost:3000/user" className="link-block">
                 View your order
             </Link>
+
+            {/*To be deleted*/}
+            <Button type="primary" danger onClick={handleCancel}>
+                Testing: Cancel
+            </Button>
 
         </div>
     )
