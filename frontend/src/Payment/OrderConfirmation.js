41c92072743a92004d745ae6c28f858cf75f2caa
nawa-ton <nawaratt.ton@gmail.com>
2021-07-16 12:50:43 -0700

    clean up code

--- frontend/src/Payment/OrderConfirmation.js
+++ frontend/src/Payment/OrderConfirmation.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import {SmileTwoTone, CheckCircleTwoTone} from "@ant-design/icons";
-import {Typography, Button} from "antd";
-import {CardElement} from "@stripe/react-stripe-js";
+import {Typography} from "antd";
 
 const { Title, Text, Link } = Typography;
-const backEndURL = "http://localhost:9999/api/";
 
 function OrderConfirmation(props) {
-
-    const handleCancel = async (e) => {
-        e.preventDefault();
-
-        const transaction = {
-            id: props.newTransaction._id,
-            status: "canceled"
-        };
-
-        fetch(backEndURL + "transaction/update_status", {
-            method: "PATCH",
-            headers: new Headers({
-                'Accept': 'application/json',
-                "Content-Type": "application/json",
-            }),
-            body: JSON.stringify(transaction)
-        }).then(res => {
-            if(res.status === 200) {
-                console.log("transaction status successfully updated");
-            }else if(res.status >= 400){
-                console.log("transaction status fail to updated");
-            }
-        });
-    };
 
     return (
         <div className="order-confirmation">
@@ -51,12 +25,6 @@ function OrderConfirmation(props) {
             <Link href="http://localhost:3000/user" className="link-block">
                 View your order
             </Link>
-
-            {/*To be deleted*/}
-            <Button type="primary" danger onClick={handleCancel}>
-                Testing: Cancel
-            </Button>
-
         </div>
     )
 }
