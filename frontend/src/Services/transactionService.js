2ab7241bf20b9e26176582d55238411078b0a2f6
nawa-ton <nawaratt.ton@gmail.com>
2021-07-28 22:10:34 -0700

    Implement cancel payment function

--- frontend/src/Services/transactionService.js
+++ frontend/src/Services/transactionService.js
@@ -6,4 +6,12 @@ axios.defaults.withCredentials = true;
 export const getTransactionHistory = async () => {
     return await axios.get(backendURL+'/api/transaction/uuid')
         .then(res => res.data);
-}
+};
+
+export const cancelTransaction = (transaction, stripe, animal) => {
+    axios.patch(backendURL+'/api/transaction/cancel', {
+        id: transaction,
+        stripeId: stripe,
+        animalId: animal
+    }).then(() => console.log("transaction canceled")).catch(err => console.log(err))
+};
