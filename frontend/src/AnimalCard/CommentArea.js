17a007cbe14e953515b7adfc504e6817fde3e86e
nawa-ton <nawaratt.ton@gmail.com>
2021-07-28 22:32:39 -0700

    Fix bug, clean up console.log

--- frontend/src/AnimalCard/CommentArea.js
+++ frontend/src/AnimalCard/CommentArea.js
@@ -17,43 +17,44 @@ export const CommentArea = (props) => {
           setUserId(res.uuid);
       }
       )
-    },[])
+    },[]);
 
     const onCommentSubmit = (values) => {
-        const commentText = values.commentText;
-        form.resetFields();
-        props.onSubmitFinish(false);
-        props.onCommentSubmit(props.id, props.type, commentText);
+        if(userId === "" || userId === undefined) {
+            showLoginRequiredModal("Please login to add your comment");
+        }else{
+            const commentText = values.commentText;
+            form.resetFields();
+            props.onSubmitFinish(false);
+            props.onCommentSubmit(props.id, props.type, commentText);
+        }
+
     };
 
     return(
         <Comment
-          avatar={
-            <Avatar
-              src={avatar}
-              alt="Han Solo"
-            />
-          }
-          content={
-            <Form
-                form = {form}
-                onFinish = {onCommentSubmit}
-            >
-                <Form.Item name="commentText">
-                    <Input.TextArea ref = {commentInput} id = "comment-text" rows={3} style = {{maxWidth:'90%', resize:'none'}}/>
-                </Form.Item>
-                <Form.Item>
-                    <Button htmlType="submit" type="primary"
-                            onClick={() => userId === "" || userId === undefined ?
-                                showLoginRequiredModal("Please login to add your comment") :
-                                onCommentSubmit()
-                            }>
-                        Add Comment
-                    </Button>
-                </Form.Item>
-            </Form>
-          }
+            avatar={
+                <Avatar
+                    src={avatar}
+                    alt="Han Solo"
+                />
+            }
+            content={
+                <Form
+                    form = {form}
+                    onFinish = {onCommentSubmit}
+                >
+                    <Form.Item name="commentText">
+                        <Input.TextArea ref = {commentInput} id = "comment-text" rows={3} style = {{maxWidth:'90%', resize:'none'}}/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button htmlType="submit" type="primary" >
+                            Add Comment
+                        </Button>
+                    </Form.Item>
+                </Form>
+            }
         />
 
     )
-}
+};
