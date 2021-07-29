d948fa23041629ccaf98d5074cbfce9602b05ac0
nawa-ton <nawaratt.ton@gmail.com>
2021-07-28 22:14:25 -0700

    Add login required popup

--- frontend/src/AnimalCard/CommentArea.js
+++ frontend/src/AnimalCard/CommentArea.js
@@ -1,16 +1,21 @@
-import { Input, Form, Button, Avatar, Comment } from 'antd'
+import {Input, Form, Button, Avatar, Comment} from 'antd'
 import React,{useEffect, useRef, useState} from 'react'
 import { getUserInfo } from '../Services/userService';
+import {showLoginRequiredModal} from "../Services/modal";
+
 
 export const CommentArea = (props) => {
     const [form] = Form.useForm();
     const [avatar, setAvatar] = useState("");
+    const [userId, setUserId] = useState("");
 
     const commentInput = useRef("");
 
     useEffect(() => {
-      getUserInfo().then(
-        res => setAvatar(res.avatar)
+      getUserInfo().then(res => {
+          setAvatar(res.avatar);
+          setUserId(res.uuid);
+      }
       )
     },[])
 
@@ -19,9 +24,7 @@ export const CommentArea = (props) => {
         form.resetFields();
         props.onSubmitFinish(false);
         props.onCommentSubmit(props.id, props.type, commentText);
-    }
-
-
+    };
 
     return(
         <Comment
@@ -40,7 +43,11 @@ export const CommentArea = (props) => {
                     <Input.TextArea ref = {commentInput} id = "comment-text" rows={3} style = {{maxWidth:'90%', resize:'none'}}/>
                 </Form.Item>
                 <Form.Item>
-                    <Button htmlType="submit" type="primary" >
+                    <Button htmlType="submit" type="primary"
+                            onClick={() => userId === "" || userId === undefined ?
+                                showLoginRequiredModal("Please login to add your comment") :
+                                onCommentSubmit()
+                            }>
                         Add Comment
                     </Button>
                 </Form.Item>
