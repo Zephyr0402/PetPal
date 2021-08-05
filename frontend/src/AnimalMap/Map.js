5e7954203a68b276d12770236e70ade244f08669
ZephyrOne <bjshenshijun@yahoo.com>
2021-08-05 03:31:04 -0700

    Fixed marker info display bug & added list on marker info if there are more than one animalinfo at the same location

--- frontend/src/AnimalMap/Map.js
+++ frontend/src/AnimalMap/Map.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import { List, Button } from 'antd';
+import ReactDOM from "react-dom";
 
 const mapStyles = {
     position: 'absolute',
@@ -10,29 +12,52 @@ const mapStyles = {
 var aid2marker = [];
 
 const AnimalMap = (props) => {
-   // const [showInfoWindow, setShowInfoWindow] = useState(false);
+    // const [showInfoWindow, setShowInfoWindow] = useState(false);
     const [activeMarker, setActiveMarker] = useState({});
     const data = props.animalCardInfo;
-    console.log("aid is:", props.aid);
+    //const aid = props.aid;
+    const [aid, setAid] = useState(-1);
+    const [markerData, setMarkerData] = useState([]);
+    
 
     useEffect(() => {
+        console.log('testingggg')
+        console.log(data); 
+        const mData = [];
+        console.log("aid is:", props.aid);
         if (props.aid > -1) {
-            aid2marker[props.aid].marker.click();
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].address === data[props.aid].address) {
+                    mData.push({
+                        "name": data[i].name,
+                        "aid": i,
+                    });
+                }
+            }
+            setMarkerData(mData);
+            setAid(props.aid);
         }
-    }, []);
+
+    }, [props.aid, props.animalCardInfo]);
+
+    // useEffect(() => {
+    //     aid = -1;
+    // }, [props.aid]);
 
     const marker2aid = (marker) => {
         for (let i in aid2marker) {
-            if(aid2marker[i].props.position === marker.position){
+            if (aid2marker[i].props.position === marker.position) {
                 return i;
             }
         }
     }
 
     const onMarkerClick = (marker) => {
+        console.log('onMarkerClick');
         props.setDisplay(marker2aid(marker));
-        //setShowInfoWindow(true);
         setActiveMarker(marker);
+        // setShowInfoWindow(true);
+        
     }
 
     const onInfoWindowClose = (props) => {
@@ -41,38 +66,75 @@ const AnimalMap = (props) => {
         // }
     }
 
-    return(
+    const onInfoWindowOpen = (props, e) => {
+        const button = (
+            <div>
+                <List style={{ width: "100%" }}
+                    itemLayout="horizontal"
+                    dataSource={markerData}
+                    renderItem={(item, index) => (
+                        <List.Item className='marker-list-item' id={item.aid} style={{ width: "100%" }}>
+                            <Button onClick={(e) => { console.log('button' + item.aid); props.setDisplay(item.aid) }} type="link" >{item.name}</Button>
+                        </List.Item>
+                    )}
+                />
+
+            </div>
+            
+        );
+        ReactDOM.render(
+            React.Children.only(button),
+            document.getElementById("iwc")
+        );
+    }
+
+    return (
         <Map
-            containerStyle = {mapStyles}
+            containerStyle={mapStyles}
             google={props.google}
             zoom={14}
-            initialCenter={{lat:49.26127572955761, lng:-123.23869115661624}}
-            center = {activeMarker.position}
+            initialCenter={{ lat: 49.26127572955761, lng: -123.23869115661624 }}
+            center={activeMarker.position}
         >
-            {data.map((ele,index) =>
-                <Marker
-                    name = {ele.name}
-                    position = {ele.position}
+            {
+            data.map((ele, index) => {
+                return <Marker
+                    name={ele.name}
+                    position={ele.position}
                     onClick={onMarkerClick}
-                    ref = {(marker) => aid2marker[index] = marker}
+                    ref={(marker) => aid2marker[index] = marker}
                 />
+            }
             )}
-            <InfoWindow
-                marker={
-                    props.aid > -1 ?
-                    aid2marker[props.aid].marker : null
-                }
-                visible={props.aid > -1}
+            {aid > -1 && 
+                <InfoWindow
+                    marker={
+                        aid2marker[aid].marker
+                    }
+                    visible={true}
                 onClose={onInfoWindowClose}
+                onOpen={e => {
+                    onInfoWindowOpen(props, e);
+                }}
             >
-                <div className="hello">
-                    <h3>{props.aid > -1 ? data[props.aid].name : ""}</h3>
-                    <pre>{props.aid > -1 ? data[props.aid].address : ""}</pre>
+                <div id="iwc">
+
+                    {/* <List style={{ width: "100%" }}
+                        itemLayout="horizontal"
+                        dataSource={markerData}
+                        renderItem={(item, index) => (
+                            <List.Item className='marker-list-item' id={item.aid} style={{ width: "100%" }}>
+                                <Button onClick={console.log("hey")} type="link" >{item.name}</Button>
+                            </List.Item>
+                        )}
+                    /> */}
                 </div>
             </InfoWindow>
+            }
         </Map>
     )
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyDnMJlodY_mrnG1k--Ol-Ocm9bWgaJF18k'})(AnimalMap);
+    apiKey: 'AIzaSyDnMJlodY_mrnG1k--Ol-Ocm9bWgaJF18k'
+})(AnimalMap);
