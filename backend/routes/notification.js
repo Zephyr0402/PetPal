74eed1e6575eebcbbbad7a1430a466de0b5f4e3a
ZephyrOne <bjshenshijun@yahoo.com>
2021-08-09 01:35:57 -0700

    Added comment notification APIs

--- backend/routes/notification.js
+++ backend/routes/notification.js
@@ -1,38 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const NotificationInfo = require('../models/notificationinfoModel');
-const AnimalInfo = require('../models/animalinfoModel');
-// {}
-router.post('/comment', async function (req, res) {
-    console.log('Update one comment notification');
-    try {
-        // Find seller id
-        const infos = await AnimalInfo.find({ "id": req.body.uaid }).populate('userinfo');
-        const sourceUserID = infos.userinfo.uuid;
-        const destinationUserID = req.session.uuid;
-        const contentID = "";
-        await NotificationInfo.create({
-            type: "comment",
-            sourceUserID: sourceUserID,
-            destinationUserID: destinationUserID,
-            contentID: contentID,
-            timestamp: Date.now()
-        })
-    } catch (err) {
-        console.log(err);
-    }
-});
 
-router.get('/comment/:dstUserID/:timestamp', function (req, res) {
+router.get('/comment/:dstUserID/:timestamp', async (req, res) => {
     console.log('Get updated comment notification');
     const dstUserID = req.params.dstUserID;
-    const timestamp = Data.parse(req.params.timestamp);
-    const infos = await NotificationInfo.find({ "destinationUserID": dstUserID });
+    const infos = await NotificationInfo.find({ "destinationUserID": dstUserID, "type": "comment" });
     var resInfos = [];
-    for (let info in infos) {
-        const infoDate = Date.parse(info.timestamp);
-        if (dates.compare(infoDate, timestamp) >= 0) {
-            resInfos.push(info);
+    if (req.params.timestamp === "empty") {
+        resInfos = infos;
+    } else {
+        const timestamp = Data.parse(req.params.timestamp);
+        for (let info in infos) {
+            const infoDate = Date.parse(info.timestamp);
+            if (dates.compare(infoDate, timestamp) >= 0) {
+                resInfos.push(info);
+            }
         }
     }
     res.send(resInfos);
