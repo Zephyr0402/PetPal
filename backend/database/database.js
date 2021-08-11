3d473c45d2a43b15a8970138c548ed090f69569c
ZephyrOne <bjshenshijun@yahoo.com>
2021-08-11 13:14:07 -0700

    Upgraded database

--- backend/database/database.js
+++ backend/database/database.js
@@ -5,7 +5,7 @@ const configs = {
     Name: 'PetPal',
 }
 
-// mongoose.connect('mongodb://' + configs.URL + '/' + configs.Name);
+//mongoose.connect('mongodb://' + configs.URL + '/' + configs.Name);
 mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@petpal.r7hhb.mongodb.net/PetPal?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 module.exports = mongoose;
