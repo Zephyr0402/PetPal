eecc59a095df75021e9f16d823592423fb2cfab3
runzw <78052475+runzw@users.noreply.github.com>
2021-10-31 14:47:38 -0700

    Test database

--- backend/database/database.js
+++ backend/database/database.js
@@ -6,6 +6,7 @@ const configs = {
 }
 
 //mongoose.connect('mongodb://' + configs.URL + '/' + configs.Name);
-mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@petpal-cpsc455.r7hhb.mongodb.net/PetPal?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@petpal-cpsc455.r7hhb.mongodb.net/PetPal?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://127.0.0.1:27017/PetPal',  {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
 module.exports = mongoose;
