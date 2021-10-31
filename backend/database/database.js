93d25e80ea266d3c9152e5d4af720a728c873b6f
runzw <78052475+runzw@users.noreply.github.com>
2021-10-31 15:25:24 -0700

    Test db

--- backend/database/database.js
+++ backend/database/database.js
@@ -7,6 +7,8 @@ const configs = {
 
 //mongoose.connect('mongodb://' + configs.URL + '/' + configs.Name);
 //mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@petpal-cpsc455.r7hhb.mongodb.net/PetPal?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-mongoose.connect('mongodb://127.0.0.1:27017/PetPal',  {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/PetPal',  {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+     .then(() => console.log( 'Database Connected' ))
+     .catch(err => console.log( err ));
 
 module.exports = mongoose;
