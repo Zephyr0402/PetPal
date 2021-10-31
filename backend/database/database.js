05e30ff1261cb2bfdf8a0ec10fe9bc9ee2be1ac5
runzw <78052475+runzw@users.noreply.github.com>
2021-10-31 16:33:22 -0700

    Test new cluster

--- backend/database/database.js
+++ backend/database/database.js
@@ -5,10 +5,14 @@ const configs = {
     Name: 'PetPal',
 }
 
+const connection_url = 'mongodb+srv://m001-student:m001-mongodb-basics@cluster0.ovnsr.mongodb.net/Petpal?retryWrites=true&w=majority';
+
 //mongoose.connect('mongodb://' + configs.URL + '/' + configs.Name);
 //mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@petpal-cpsc455.r7hhb.mongodb.net/PetPal?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-mongoose.connect('mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb',  {useNewUrlParser: true})
-     .then(() => console.log( 'Database Connected' ))
-     .catch(err => console.log( err ));
+mongoose.connect(connection_url, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+// mongoose.connect('mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb',  {useNewUrlParser: true})
+//      .then(() => console.log( 'Database Connected' ))
+//      .catch(err => console.log( err ));
 
 module.exports = mongoose;
