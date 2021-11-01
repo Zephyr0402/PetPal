bf4ef88cf991cbf57679af78279c13acfd5a6829
runzw <78052475+runzw@users.noreply.github.com>
2021-11-01 01:03:29 -0700

    Update database.js

--- backend/database/database.js
+++ backend/database/database.js
@@ -5,7 +5,7 @@ const configs = {
     Name: 'PetPal',
 }
 
-const connection_url = 'mongodb+srv://m001-student:<password>@cluster0.ovnsr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const connection_url = 'mongodb+srv://m001-student:m001-mongodb-basics@cluster0.ovnsr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 //mongoose.connect('mongodb://' + configs.URL + '/' + configs.Name);
 //mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@petpal-cpsc455.r7hhb.mongodb.net/PetPal?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
